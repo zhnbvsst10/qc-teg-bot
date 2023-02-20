27329c24b4c1a4f5a0f44eeea86ba804f8818a6a
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-02-21 00:26:05 +0600

    other message

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import asyncio
 import logging
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import Bot, Dispatcher
+import aiogram
+from aiogram import Bot
+from aiogram import Dispatcher
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from aiogram.fsm.storage.memory import MemoryStorage
 from handlers import common, pvc_1, pvc_3
-#from aiogram.utils.web_app import stexecutor import start_webhook
+from aiohttp.web_app import Application
+from aiohttp.web import run_app
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 import os
 
 token = '6029120908:AAFSntMGALPmIV7eJPrLbPxXEk_pEEdTRgQ'
@@ -12,8 +17,8 @@ WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{token}'
 WEBHOOK_URL = f'jdafhjasdh2u3alskd/{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-dp = Dispatcher(storage=MemoryStorage())
+WEBAPP_PORT = os.getenv('PORT', default=8081)
+dp = Dispatcher()
 bot = Bot(token=token)
 
 async def on_startup(dispatcher):
@@ -35,14 +40,10 @@ async def main():
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
-    token = '6029120908:AAFSntMGALPmIV7eJPrLbPxXEk_pEEdTRgQ'
-    dp = Dispatcher(storage=MemoryStorage())
-    bot = Bot(token=token)
     sched = AsyncIOScheduler({'apscheduler.timezone':'Asia/Almaty'})
     sched.add_job(send_message,'cron',hour='8-20/1', minute = '30', kwargs= {'bot':bot} )
     sched.start()
     sched.print_jobs()
-    dp.update.middleware.register(SchedulerMiddleware(sched))
     dp.include_router(common.router)
     dp.include_router(pvc_1.router)
     dp.include_router(pvc_3.router2)
@@ -51,14 +52,13 @@ async def main():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    start_webhook(
+    app = Application()
+    app["bot"] = bot
+    SimpleRequestHandler(
         dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+        bot=bot,
+    ).register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    run_app(app, host="0.0.0.0", port=8081)
     asyncio.run(main())
     
