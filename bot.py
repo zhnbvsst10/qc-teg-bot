7dd1b91fddc13dc788d67b983fb5a190a169b759
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-02-20 23:04:46 +0600

    changed functions

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ token = '6029120908:AAFSntMGALPmIV7eJPrLbPxXEk_pEEdTRgQ'
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{token}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_URL = f'jdafhjasdh2u3alskd/qc-teg-bot'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 dp = Dispatcher(storage=MemoryStorage())
