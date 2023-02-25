f266c1d2f064cc9804fb6a9690cfdd973fb4063d
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-02-26 00:20:51 +0600

    photo saving

--- bot.py
+++ bot.py
@@ -42,8 +42,8 @@ async def main():
 
     await dp.start_polling(bot, skip_updates=True)
 
-@common.router.message()
-async def get_photo(message: Message.photo):
+@common.router.message(content_types=['photo'])
+async def get_photo(message: Message):
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()           
     drive = GoogleDrive(gauth)  
