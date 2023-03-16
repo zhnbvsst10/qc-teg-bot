28645059b0d94fab96db79da83e6db0b83fc07b2
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-03-17 00:53:20 +0600

    new logic

--- keyboards/simple_row.py
+++ keyboards/simple_row.py
@@ -9,3 +9,12 @@ def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
     """
     row = [KeyboardButton(text=item) for item in items]
     return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+def make_row_keyboard_2(items: list[str]) -> ReplyKeyboardMarkup:
+    """
+    Создаёт реплай-клавиатуру с кнопками в один ряд
+    :param items: список текстов для кнопок
+    :return: объект реплай-клавиатуры
+    """
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row])
