f7d11e7a14d9358b8706fb9c3645bcc893eb9edb
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-04-02 01:04:16 +0600

    new one

--- bot.py
+++ bot.py
@@ -25,6 +25,9 @@ async def send_message(bot: Bot):
     await bot.send_message(1051813835,'Проверка контроля качества. \n Перейти в /start')
     await bot.send_message(1374864950, 'Проверка контроля качества. \n Перейти в /start')
     
+async def send_night_message(bot: Bot):
+    await bot.send_message(1309686262, 'Проверка контроля качества. \n Перейти в /start')
+    
 async def main():
     bot.delete_webhook()
     logging.basicConfig(
@@ -34,6 +37,8 @@ async def main():
     
     sched = AsyncIOScheduler({'apscheduler.timezone':'Asia/Almaty'})
     sched.add_job(send_message,'cron',hour='8-20/1', minute = '30', kwargs= {'bot':bot} )
+    sched.add_job(send_night_message,'cron',hour='21-23/1', minute = '30', kwargs= {'bot':bot} )
+    sched.add_job(send_night_message,'cron',hour='0-8/1', minute = '30', kwargs= {'bot':bot} )
     sched.start()
     sched.print_jobs()
     dp.include_router(common.router)
