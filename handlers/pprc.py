8af9dbac77011177c2e52a39205393fbe2eb8238
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-04-20 12:16:20 +0600

    pprc

--- handlers/pprc.py
+++ handlers/pprc.py
@@ -193,6 +193,7 @@ async def get_photo_pprc_view(message: Message, state: FSMContext):
     gauth.LocalWebserverAuth()           
     drive = GoogleDrive(gauth)  
     file_id =  message.photo[-1].file_id
+    print(message.photo[-1])
     file_unique_id = message.photo[-1].file_unique_id
     PhotoSize(file_id=file_id, file_unique_id=file_unique_id, width='1920', height='1080')
     file = await bot.get_file(file_id)
@@ -203,10 +204,10 @@ async def get_photo_pprc_view(message: Message, state: FSMContext):
     for upload_file in upload_file_list:
         gfile = drive.CreateFile({'parents': [{'id': '1VnkFYt-wgCIyaEDoYUsOjjkYP0BzXQcE'}]})
         gfile.Upload()
-    # await message.answer(
-    #         text="продолжить",
-    #         reply_markup=make_row_keyboard(['yes'])
-    #         )
+    await message.answer(
+            text="продолжить",
+            reply_markup=make_row_keyboard(['yes'])
+            )
     await state.set_state(SetParameterPPRC.send_photo_view_sent)
 
 @router.message(SetParameterPPRC.send_photo_view_sent)
