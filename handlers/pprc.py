81ef149cea8835e7d4a7645e72018c5b0d2262ba
zhnbvsst10 <asset.zhanabayev@nu.edu.kz>
2023-04-20 12:09:33 +0600

    pprc

--- handlers/pprc.py
+++ handlers/pprc.py
@@ -197,7 +197,7 @@ async def get_photo_pprc_view(message: Message, state: FSMContext):
     PhotoSize(file_id=file_id, file_unique_id=file_unique_id, width='1920', height='1080')
     file = await bot.get_file(file_id)
     file_path = file.file_path
-    filename = 'pprc_view' + (datetime.now() + timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S' + '.jpg')
+    filename = 'pprc_view_' + (datetime.now() + timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S' + '.jpg')
     await bot.download_file(file_path, filename )
     upload_file_list = [filename]
     for upload_file in upload_file_list:
